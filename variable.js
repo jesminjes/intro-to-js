ce5df61511a4ffe9f19e8fc2891ac629f64e8f93
jesminjes <mstjesmina56@gmail.com>
2022-06-16 16:00:44 +0600

    true false

--- variable.js
+++ variable.js
@@ -1,3 +1,12 @@
 var price = 21;
 var age = 19;
 var tenperature = 34;
+
+var name = "jesmin akter";
+var address = "thakurgaon road";
+var scholl = "ideal scholl and college";
+
+var pass = true;
+var pass = true;
+var subscribed = false;
+var love = false;
