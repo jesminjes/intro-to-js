067282cae470e5602614cc506971c3e178c22399
jesminjes <mstjesmina56@gmail.com>
2022-06-16 21:34:17 +0600

    camel case

---
+++ variable-name.js
@@ -0,0 +1,18 @@
+var name = "Jesmin Akter";
+
+// reserved keyword not allowed
+var  false = 10;
+var delet = "delete";
+
+// name should be one word
+var nayok name = 'TOM HANKS';
+
+// quote is not allowed
+var "nayka" = "shabnoor";
+
+// camel case
+var myOfficeAddress = "ijwot utiow ;"
+userHomeAddress = 'katabon';
+
+var PIE = 3.14;
+
