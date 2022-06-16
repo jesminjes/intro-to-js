8313cacba7a894a06d929bcba3181766819e7f8d
jesminjes <mstjesmina56@gmail.com>
2022-06-16 22:04:53 +0600

    quantity

--- mathematical-operation.js
+++ mathematical-operation.js
@@ -1,8 +1,20 @@
 var onionPrice = 43;
 var eggPrice = 42;
+var eggquantity = 7;
 var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var eggPriceMultiplication = eggPrice *eggquantity;
+
 
 console.log(onionPrice);
 console.log(eggPrice);
 console.log(onionPrice + eggPrice);
 console.log(totalPrice);
+console.log(priceDifference);
+console.log(eggPriceMultiplication);
+
+
+var moneyAvaiable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvaiable / orangePrice;
+console.log(orangeQuantity);
