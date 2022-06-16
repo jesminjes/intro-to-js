d3ccec8294bd9a94aa1e2dea4a0805c9335a990b
jesminjes <mstjesmina56@gmail.com>
2022-06-17 00:19:41 +0600

    age

---
+++ math-operation.js
@@ -0,0 +1,18 @@
+// recap
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var divission = price1 / price2;
+
+console.log(sum);
+console.log(difference);
+console.log(multiplication);
+console.log(divission);
+var doublePrice = price2 * 20;
+price += 10;
+console.log(price2);
+var age = 14;
+age = age +1;
+console.log(age);
