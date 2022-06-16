fc7020d47e828639efb7caf6a568aaea190ab9d6
jesminjes <mstjesmina56@gmail.com>
2022-06-16 21:47:44 +0600

    console log

---
+++ mathematical-operation.js
@@ -0,0 +1,8 @@
+var onionPrice = 43;
+var eggPrice = 42;
+var totalPrice = onionPrice + eggPrice;
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
