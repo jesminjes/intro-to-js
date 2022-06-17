ed3b3527cd15f51fb65cf6e70dae5c65c70f04aa
jesminjes <mstjesmina56@gmail.com>
2022-06-17 10:26:47 +0600

    shorthand

---
+++ math-shorthand.js
@@ -0,0 +1,28 @@
+// recap
+var price1 = 31;
+var price2 = 10;
+var sum = price1 + price2;
+var difference = price2 - price1;
+var multiplication = price1 * price2;
+var divission = price1 / price2;
+
+// console.log(sum);
+// console.log(difference);
+// console.log(multiplication);
+// console.log(divission);
+
+var doublePrice = price2 * 20;
+price2 += 10;
+// console.log(price2);
+var age = 14;
+age = age +1;
+age += 1;
+age++;
+
+// console.log(age);
+
+var love = 100;
+// love = love - 1;
+// love -= 1;
+love--;
+console.log(love);
