2f1b543db9802048ebeac9c2322da0da9d538eb2
jesminjes <mstjesmina56@gmail.com>
2022-06-17 11:28:10 +0600

    typeof date

---
+++ variable-types.js
@@ -0,0 +1,16 @@
+var tShirtPrice = 200;
+// c
+onsole.log(typeof tShirtPrice);
+
+var tShirtPrice2 = '200';
+// console.log(typeof tShirtPrice2);
+
+var isRaining = true;
+console.log(typeof isRaining);
+var isRomantic;
+// console.log(isRomantic);
+
+var today = new Date();
+console.log(typeof  today);
+
+
