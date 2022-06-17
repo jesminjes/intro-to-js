2ece9adf215cb1acdd3d6b710281195d2ad4bedc
jesminjes <mstjesmina56@gmail.com>
2022-06-17 10:56:34 +0600

    float int

---
+++ conversion.js
@@ -0,0 +1,33 @@
+var firstName = 'Jesmin';
+var lastName = 'Akter';
+var fullName = firstName + ' ' + lastName; 
+// console.log(fullName);
+
+var first = 'Sahinur';
+var last = 'Islam';
+var name = first + ' ' + last;
+// console.log(name);
+
+
+var onionPrice = '32';
+var eggPrice = '39';
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+
+// integer
+var sunglass = 3;
+
+// float
+var price = 99.99;
+
+var onionPrice = '32';
+var eggPrice = '39.55';
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(onionPriceNumber);
+console.log(eggPriceNumber);
+
+
+
+
