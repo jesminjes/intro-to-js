7ae579a34ac835179129ac45241246434b222a4a
jesminjes <mstjesmina56@gmail.com>
2023-03-25 12:11:26 +0600

    true-false

--- variable.js
+++ variable.js
@@ -1,12 +1,21 @@
+//***numeric
 var price = 21;
 var age = 19;
 var tenperature = 34;
 
+
+//***string
 var name = "jesmin akter";
 var address = "thakurgaon road";
-var scholl = "ideal scholl and college";
+var school = "islam nagor high school";
+var location = "thakurgaon";
+var special = "alia bahtt";
 
+//**b00lean 
 var pass = true;
 var pass = true;
 var subscribed = false;
 var love = false;
+var serious = true;
+var isFullMarks = true;
+var isSingle = false;
