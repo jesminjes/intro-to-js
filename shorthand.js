6c7884cd0eef0b724d6365994ac0a2414692728d
jesminjes <mstjesmina56@gmail.com>
2023-03-25 19:30:27 +0600

    shorthand

---
+++ shorthand.js
@@ -0,0 +1,16 @@
+var price1 = 10;
+var price2 = 20;
+// console.log(price1);
+
+ price1 = price1 + 10;
+ console.log(price1);
+
+ price1 += 20;
+ console.log(price1);
+
+ price2 *= 10;
+ console.log(price2);
+
+
+  price2 /= 20;
+  console.log(price2);
