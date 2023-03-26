38957ff28288bec30e140a25c656434691062f30
jesminjes <mstjesmina56@gmail.com>
2023-03-26 12:11:06 +0600

    advanced

--- variable-types.js
+++ variable-types.js
@@ -7,5 +7,13 @@ console.log(typeof price);
 var isHappy = true;
 console.log(typeof isHappy);
 
-
+var romantic;
+console.log(typeof romantic);
+// advanced
+var num1 = 0.1;
+var num2 = 0.2;
+var sum = num1 + num2;
+sum = sum.toFixed(3);
+console.log(sum);
+sum = parseFloat(sum);
 
